898f9cfc0583c390cec8311ba64622aaae66ba04
BenjamimEterino <benjamimtoretto901@gmail.com>
2024-07-02 23:11:15 +0200

    as

--- src/models/requisicao/useCases/approveRequisicao/ApproveRequsicaoUC.ts
+++ src/models/requisicao/useCases/approveRequisicao/ApproveRequsicaoUC.ts
@@ -1,5 +1,6 @@
-import { inject, injectable } from "tsyringe";
+import { container, inject, injectable } from "tsyringe";
 import { IRequisicao } from "../../repositories/IRequisicao";
+import { CreateNotifiUC } from "../../../notification/useCase/createNotifi/CreateNotifiUC";
 
 @injectable()
 class ApproveRequisicaoUC {
@@ -11,6 +12,10 @@ class ApproveRequisicaoUC {
     async execute(id_requisicao: string) {
         const requisicao = await this.requisicaoRepo.approveReq(id_requisicao)
 
+        const notifiUC = container.resolve(CreateNotifiUC)
+
+        await notifiUC.execute('8699a102-6881-4a17-90ec-ec582e3dbf71', new Date(requisicao.data), requisicao.status)
+        
         return requisicao
     }
 }
--- src/models/requisicao/useCases/deliverRequisicao/DeliverRequisicaoUC.ts
+++ src/models/requisicao/useCases/deliverRequisicao/DeliverRequisicaoUC.ts
@@ -1,5 +1,6 @@
-import { inject, injectable } from "tsyringe";
+import { container, inject, injectable } from "tsyringe";
 import { IRequisicao } from "../../repositories/IRequisicao";
+import { CreateNotifiUC } from "../../../notification/useCase/createNotifi/CreateNotifiUC";
 
 @injectable()
 class DeliverRequisicaoUC{
@@ -10,6 +11,10 @@ class DeliverRequisicaoUC{
     
     async execute(id_requisicao: string) {
         const requisicao = await this.requisicaoRepo.deliverReq(id_requisicao)
+
+        const notifiUC = container.resolve(CreateNotifiUC)
+
+        await notifiUC.execute('8699a102-6881-4a17-90ec-ec582e3dbf71', new Date(requisicao.data), requisicao.status)
 
         return requisicao
     }
